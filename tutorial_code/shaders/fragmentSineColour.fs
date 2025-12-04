260ec49f9f1efc76cde2654a62fb1eb69aeeaa17
DominikMat <dominikmat@student.agh.edu.pl>
2025-12-04 21:00:30 +0200

    opengl engine

--- tutorial_code/shaders/fragmentSineColour.fs
+++
@@ -1,8 +0,0 @@
-#version 330 core
-in vec3 ourColor;
-out vec4 FragColor;
-uniform float timeMult;
-void main()
-{
-   FragColor = vec4(ourColor.x*timeMult,ourColor.y*timeMult,ourColor.z*timeMult, 1.0f);
-}
